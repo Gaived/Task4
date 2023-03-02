49ea2c72654d2cc38cbf6d79e4de67b9ba030ef2
Ivan Andreev <fc97@mail.ru>
2023-03-03 00:27:27 +0400

    Завершение создания программы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MagicBox<String> box1 = new MagicBox<>(4);
+        MagicBox<Integer> box2 = new MagicBox<>(2);
+
+        box2.add(1);
+        box2.add(2);
+        box2.pick();
+
+        box1.add("Один");
+        box1.add("Два");
+        box1.pick();
+        box1.add("Три");
+        box1.add("Четыре");
+        //box1.pick();
+        box1.add("Пять");
+
     }
 }
